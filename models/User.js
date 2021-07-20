d77ebd427f9202d07ecfe35cff8e6ab641f16fb6
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-19 18:26:15 -0700

    updated routes

--- models/User.js
+++ models/User.js
@@ -1,14 +1,14 @@
-const bcrypt = require("bcrypt");
 const { Model, DataTypes } = require('sequelize');
-const { beforeUpdate } = require("../config/connection");
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
-
-bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-  // store hash in your password DB.
-})
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
@@ -43,10 +43,10 @@ User.init(
     hooks: {
       // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
-      // set up beforeCreate lifecycle "hook" functionality
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
