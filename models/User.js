e22e8551f220e2f7849ae2d21cf20ec5e637d0b1
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-19 18:07:01 -0700

    code isn't working server isn't connecting

--- models/User.js
+++ models/User.js
@@ -1,63 +1,63 @@
-const { Model, Datatypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+const { Model, DataTypes } = require('sequelize');
+const { beforeUpdate } = require("../config/connection");
+const sequelize = require('../config/connection');
 
-// Create our user model
+// create our User model
 class User extends Model {}
 
-// define table columns and configuration
-User.init(
-    {
-        // define an id column
-        id: {
-            // use the special Sequelize Datatypes object to provide what type of data it is
-            type: Datatypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
-            allowNull: false,
-            // instruct that this is the Primary Key
-            primaryKey: true,
-            // turn on auto increment
-            autoIncrement: true
-        },
-        // define a username column
-        username: {
-            type: Datatypes.STRING,
-            allowNull: false
-        },
-        // define an email column
-        email: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            // there cannot be any duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
-            validate: {
-                isEmail: true
-            }
-        },
-        // define a password column
-        password: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                // this means the password must be at least four characters long
-                len: [4]
-            }
-        }
-    },
-    {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+  // store hash in your password DB.
+})
 
-        // pass in our imported sequelize connection (the direct connection to our database) 
-        sequelize,
-        // don't automatically create createdAt/updatedAt tomestamp fields
-        timestamps: false,
-        // don't pluralize name of database table
-        freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment-text` and not `commentText`) 
-        underscored: true,
-        // make it so our model name stays lowercase in the database
-        modelName: "user"
+// create fields/columns for User model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
-)
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+      },
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
 
 module.exports = User;
