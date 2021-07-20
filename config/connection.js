685dc56672ed0882242db518ed8b693dc93be123
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-20 04:15:49 -0700

    finally works

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize("just_tech_news_db", "root", "password", {
+  host: "localhost",
+
+  port: 3306,
+  dialect: "mysql",
 });
 
 module.exports = sequelize;
